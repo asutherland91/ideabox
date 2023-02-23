5f7ef3c58fdc402083f134cdc789d6b4d6f4480c
Denzel Turambi <dturambi26@gmail.com>
2023-02-23 14:57:14 -0700

    Add query selectors for buttons

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 var ideas = [];
+
+//query selector go here:
+var showStarredIdeas = document.querySelector(".show-star-btn");
+var saveButton = document.querySelector(".save-btn");
+var searchButton = document.querySelector(".search-btn");
+var starButton = document.querySelector(".star-btn");
+var deleteButton = document.querySelector(".delete-btn");
+var commentButton = document.querySelector(".comment-btn");
