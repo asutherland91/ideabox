274bc3a0fe2bc8ffe8257b801c1a3c3e96d7d77a
Randy Cisneros <rlcisneros90@gmail.com>
2023-02-27 14:42:14 -0700

    Refactor HTML, JS, and CSS

--- main.js
+++ main.js
@@ -1,56 +1,51 @@
 var ideas = [];
 
 //query selectors:
-var showStarredIdeas = document.querySelector(".show-star-btn");
 var saveButton = document.querySelector(".save-btn");
-var searchButton = document.querySelector(".search-btn");
 var deleteButton = document.querySelector(".idea-cards");
-var ideaTitle = document.querySelector(".idea-title");
-var ideaBody = document.querySelector(".idea-body");
 var formTitle = document.querySelector("#title-form");
 var formBody = document.querySelector("#body-form");
+var ideaTitle = document.querySelector(".idea-title");
+var ideaBody = document.querySelector(".idea-body");
 var ideaCards = document.querySelector(".idea-cards");
 
 //event listeners:
-saveButton.addEventListener("click", saveIdea);
 formTitle.addEventListener("keyup", toggleButton);
 formBody.addEventListener("keyup", toggleButton);
+ideaCards.addEventListener("click", favoriteIdea);
+saveButton.addEventListener("click", saveIdea);
 deleteButton.addEventListener("click", function(event){
   deleteIdea(event);
 });
-ideaCards.addEventListener("click", favoriteIdea);
-showStarredIdeas.addEventListener("click", starConstruction)
 
 //functions:
-function toggleButton() {
-  if(formTitle.value && formBody.value) {
-    saveButton.removeAttribute("disabled");
-  } else {
-    saveButton.setAttribute("disabled", "");
+  function toggleButton() {
+    if(formTitle.value && formBody.value) {
+      saveButton.removeAttribute("disabled");
+    } else {
+      saveButton.setAttribute("disabled", "");
+    }
   }
-}
 
-function addIdeaCard(idea) {
-  var ideaCard = document.createElement("section");
-  ideaCard.classList.add("card");
-  ideaCard.setAttribute("id", idea.id);
-  ideaCard.innerHTML = 
-    `<div class="top-bar">
-      <button class="star-btn" type="submit">
-        <img class="star-favorite" src="assets/star.svg" alt="star button">
-      </button>
-      <button class="delete-btn" id="deleteBtn"></button>
-    </div>
-    <div class="body-section">
-      <h3 class="idea-title">${idea.title}</h3>
-      <p class="idea-body">${idea.body}</p>
-    </div>
-    <div class="bottom-bar">
-      <button class="comment-btn" type="submit"><img src="assets/comment.svg" alt="comment button"></button>
-      <label for="comment-btn">Comment</label>
-    </div>`;
-  ideaCards.appendChild(ideaCard);
-}
+  function addIdeaCard(idea) {
+    var ideaCard = document.createElement("section");
+    ideaCard.classList.add("card");
+    ideaCard.setAttribute("id", idea.id);
+    ideaCard.innerHTML = 
+      `<div class="top-bar">
+        <button class="star-btn" type="submit">
+          <img class="star-favorite" src="assets/star.svg" alt="star button">
+        </button>
+        <button class="delete-btn" id="deleteBtn"></button>
+      </div>
+      <div class="body-section">
+        <h3 class="idea-title">${idea.title}</h3>
+        <p class="idea-body">${idea.body}</p>
+      </div>
+      <div class="bottom-bar">
+      </div>`;
+    ideaCards.appendChild(ideaCard);
+  }
 
   function saveIdea(event) {
     event.preventDefault();
@@ -59,7 +54,8 @@ function addIdeaCard(idea) {
     addIdeaCard(idea);
     clearInputFields();
     toggleButton();
-}
+  }
+
   function deleteIdea(event) {
     if(event.target.className === "delete-btn") {
       var cardToRemove = event.target.parentNode.parentNode;
@@ -75,8 +71,8 @@ function addIdeaCard(idea) {
 
   function favoriteIdea(event) {
     if(event.target.className === "star-favorite") {
-      var starFavoriteButton = document.querySelector(".star-favorite");
       var cardToFavorite = event.target.parentNode.parentNode.parentNode;
+      var starFavoriteButton = cardToFavorite.querySelector(".star-favorite");
       var cardId = Number(cardToFavorite.id);
       for (var i = 0; i < ideas.length; i++) {
         if (cardId === ideas[i].id && ideas[i].star === false) {
@@ -90,12 +86,8 @@ function addIdeaCard(idea) {
     }
   }
 
-function clearInputFields(){
-    formTitle.value = '';
-    formBody.value = '';
-}
-
-function starConstruction(){
-  alert('This feature is under construction!!')
-}
+  function clearInputFields() {
+    formTitle.value = "";
+    formBody.value = "";
+  }
 
